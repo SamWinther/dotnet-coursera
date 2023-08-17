660025a1dfa5bb0cfeaf36ec704808dbf2cad134
Hossein Tarokh <hta@LINAMED.LOCAL>
2023-08-17 09:24:16 +0200

    Testing how to make a new route. The new route is named WhatName, it was so easy to make a route. I just needed to add a controller under the folder Controllers. if the controller name has the word controller at the end, the compiler automatically drops that word. so for example if you have a controller with the name myUrlController, your new route would be myUrl

---
+++ MvcMovie/Controllers/Whatname.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class Whatname : Controller
+{
+    // 
+    // GET: /HelloWorld22/
+    public string Index()
+    {
+        return "This is easyroute...";
+    }
+    // 
+    // GET: /HelloWorld/Welcome2/ 
+    public string Welcomeee()
+    {
+        return "This is the really easy...";
+    }
+}
